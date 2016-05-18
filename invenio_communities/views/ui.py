8d98a20e550641045ca6fbd36d4de337beabdd40
remileduc <remi.ducceschi@gmail.com>
2016-05-18 10:27:01 +0200

    permission management: added several permissions
    - we can manage each community with these permissions:
      - create
      - delete
      - manage the team
      - read
      - edit
      - curate

--- invenio_communities/views/ui.py
+++ invenio_communities/views/ui.py
@@ -27,7 +27,7 @@
 from __future__ import absolute_import, print_function
 
 import copy
-from functools import wraps
+from functools import partial, wraps
 
 from flask import Blueprint, abort, current_app, flash, jsonify, redirect, \
     render_template, request, url_for
@@ -38,10 +38,12 @@ from invenio_indexer.api import RecordIndexer
 from invenio_pidstore.resolver import Resolver
 from invenio_records.api import Record
 
+from invenio_access.models import ActionUsers
 from invenio_communities.forms import CommunityForm, DeleteCommunityForm, \
     EditCommunityForm, SearchForm
 from invenio_communities.models import Community, FeaturedCommunity
-from invenio_communities.proxies import current_permission_factory
+from invenio_communities.permissions import CommunityCurateActionNeed
+from invenio_communities.proxies import current_permission_factory, needs
 from invenio_communities.utils import Pagination, render_template_to_string
 
 
@@ -52,6 +54,33 @@ blueprint = Blueprint(
     template_folder='../templates',
     static_folder='../static',
 )
+
+
+def _get_needs(action, community=""):
+    """
+    :param action: the action to execute (i.e. "communities-read")
+    :type action: str
+    :param community: the community
+    :type community: str
+    :returns: the need associated with the action
+    """
+    if community:
+        return needs[action](community)
+    return needs[action]()
+
+
+def _get_permission(action, community=""):
+    """
+    :param action: the action to execute (i.e. "communities-read")
+    :type action: str
+    :param community: the community
+    :type community: str
+    :returns: permission the permission associated to this action with
+        this community as a parameter
+    """
+    if community:
+        return current_permission_factory[action](community)
+    return current_permission_factory[action]()
 
 
 def pass_community(f):
@@ -70,10 +99,23 @@ def permission_required(action):
     def decorator(f):
         @wraps(f)
         def inner(community, *args, **kwargs):
-            permission = current_permission_factory(community, action=action)
+            permission = _get_permission(action, community)
             if not permission.can():
                 abort(403)
             return f(community, *args, **kwargs)
+        return inner
+    return decorator
+
+
+def permission_required_no_id(action):
+    """Decorator to require permission."""
+    def decorator(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            permission = _get_permission(action)
+            if not permission.can():
+                abort(403)
+            return f(*args, **kwargs)
         return inner
     return decorator
 
@@ -104,12 +146,16 @@ def index():
 
     so = so or current_app.config.get('COMMUNITIES_DEFAULT_SORTING_OPTION')
 
-    communities = Community.filter_communities(p, so)
+    communities = Community.filter_communities(p, so).all()
+    for i, c in enumerate(communities):
+        permission = _get_permission("communities-read", c)
+        if not permission.can():
+            del communities[i]
     featured_community = FeaturedCommunity.get_featured_or_none()
     form = SearchForm(p=p)
     per_page = 10
     page = max(page, 1)
-    p = Pagination(page, per_page, communities.count())
+    p = Pagination(page, per_page, len(communities))
 
     ctx.update({
         'r_from': max(p.per_page * (p.page - 1), 0),
@@ -118,9 +164,9 @@ def index():
         'pagination': p,
         'form': form,
         'title': _('Communities'),
-        'communities': communities.slice(
-            per_page * (page - 1), per_page * page).all(),
+        'communities': communities[per_page * (page - 1):per_page * page],
         'featured_community': featured_community,
+        'permission_curate': partial(_get_permission, "communities-curate")
     })
 
     return render_template(
@@ -131,6 +177,7 @@ def index():
 
 @blueprint.route('/<string:community_id>/', methods=['GET'])
 @pass_community
+@permission_required('communities-read')
 def detail(community):
     """Index page with uploader and list of existing depositions."""
     return generic_item(community, "invenio_communities/detail.html")
@@ -138,6 +185,7 @@ def detail(community):
 
 @blueprint.route('/<string:community_id>/search', methods=['GET'])
 @pass_community
+@permission_required('communities-read')
 def search(community):
     """Index page with uploader and list of existing depositions."""
     return generic_item(
@@ -148,6 +196,7 @@ def search(community):
 
 @blueprint.route('/<string:community_id>/about/', methods=['GET'])
 @pass_community
+@permission_required('communities-read')
 def about(community):
     """Index page with uploader and list of existing depositions."""
     return generic_item(community, "invenio_communities/about.html")
@@ -169,6 +218,7 @@ def generic_item(community, template, **extra_ctx):
 
 @blueprint.route('/new/', methods=['GET', 'POST'])
 @login_required
+@permission_required_no_id('communities-create')
 def new():
     """Create a new community."""
     form = CommunityForm(request.values)
@@ -213,7 +263,7 @@ def new():
 @blueprint.route('/<string:community_id>/edit/', methods=['GET', 'POST'])
 @login_required
 @pass_community
-@permission_required('community-edit')
+@permission_required('communities-edit')
 def edit(community):
     """Create or edit a community."""
     form = EditCommunityForm(request.values, community)
@@ -251,7 +301,7 @@ def edit(community):
 @blueprint.route('/<string:community_id>/delete/', methods=['POST'])
 @login_required
 @pass_community
-@permission_required('community-delete')
+@permission_required('communities-delete')
 def delete(community):
     """Delete a community."""
     deleteform = DeleteCommunityForm(request.values)
@@ -275,7 +325,7 @@ def delete(community):
 @blueprint.route('/<string:community_id>/curate/', methods=['GET', 'POST'])
 @login_required
 @pass_community
-@permission_required('community-curate')
+@permission_required('communities-curate')
 def curate(community):
     """Index page with uploader and list of existing depositions.
 
@@ -314,3 +364,4 @@ def curate(community):
         current_app.config['COMMUNITIES_CURATE_TEMPLATE'],
         **ctx
     )
+
