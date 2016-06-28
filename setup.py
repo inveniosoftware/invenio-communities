b6fbb723b2493ecae47737b02ce01c94bec5bce3
Tibor Simko <tibor.simko@cern.ch>
2016-06-28 14:49:26 +0200

    global: inveniosoftware.org
    * Changes `invenio-software.org` to `inveniosoftware.org` to use the
      same dashless canonical ID everywhere (GitHub, Twitter, Web).
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(
     keywords='invenio communities',
     license='GPLv2',
     author='CERN',
-    author_email='info@invenio-software.org',
+    author_email='info@inveniosoftware.org',
     url='https://github.com/inveniosoftware/invenio-communities',
     packages=packages,
     zip_safe=False,
