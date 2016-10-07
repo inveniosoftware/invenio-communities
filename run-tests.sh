0145e8e4f7349bfd6902b10cd624b54887628ab1
Krzysztof Nowak <k.nowak@cern.ch>
2016-10-08 01:03:02 +0200

    setup: isort glob fix
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -22,8 +22,8 @@
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-pydocstyle invenio_communities && \
-isort -rc -c -df **/*.py && \
+pydocstyle invenio_communities tests && \
+isort -rc -c -df && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
