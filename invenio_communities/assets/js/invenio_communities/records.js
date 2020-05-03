b284e050fb1e3c9d1f7975b5fd7330654de41a9e
Alexander Ioannidis <a.ioannidis@cern.ch>
2020-05-04 00:58:55 +0200

    views: replace record modal with dropdown

--- invenio_communities/assets/js/invenio_communities/records.js
+++ invenio_communities/assets/js/invenio_communities/records.js
@@ -6,14 +6,16 @@ import "semantic-ui-css/semantic.min.css";
 import {
   Accordion,
   Image,
+  Divider,
+  Input,
   Icon,
+  List,
   Form,
+  Message,
   TextArea,
   Button,
   Dropdown,
-  Modal,
 } from "semantic-ui-react";
-
 
 const useInvenioSearchAPI = (baseURL) => {
   const [query, setQuery] = useState("");
@@ -38,177 +40,244 @@ const useInvenioSearchAPI = (baseURL) => {
   return [{ query, data, isLoading, isError }, setQuery];
 };
 
-const CommunitiesSearchDropdown = ({ onSelect }) => {
+const CommunitiesRequestDropdown = () => {
+  const [community, setCommunity] = useState(null);
+  const [message, setMessage] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
+  const [response, setResponse] = useState(null);
   const [{ query, data, isLoading, isError }, setQuery] = useInvenioSearchAPI(
     "/api/communities"
   );
 
-  const handleChange = (e, { searchQuery, value }) => {
-    setQuery(searchQuery);
-    onSelect(value);
-  };
-  const handleSearchChange = (e, { searchQuery }) => setQuery(searchQuery);
-
-  return (
-    <Dropdown
-      fluid
-      onChange={handleChange}
-      onSearchChange={handleSearchChange}
-      options={data.hits.hits.map((community) => ({
-        key: community.metadata.id,
-        text: community.metadata.title,
-        value: community,
-      }))}
-      placeholder="Search communities..."
-      search
-      searchQuery={query}
-      selection
-      value=""
-    />
-  );
-};
-
-const CommunityRequestModal = () => {
-  const [open, setOpen] = useState(false);
-  const [community, setCommunity] = useState(null);
-  const [message, setMessage] = useState("");
-  const handleSelect = (community) => {
-    setCommunity(community);
-  };
   const handleSubmit = () => {
-    const postData = async () => {
-      try {
-        const result = await axios.post(
-          `/api/communities/${community.metadata.id}/requests/inclusion`,
-          {
-            record_pid: __RECORD.recid,
-            message: message,
-          }
-        );
-        console.log(result.data);
-        setOpen(false);
+    axios
+      .post(`/api/communities/${community.metadata.id}/requests/inclusion`, {
+        record_pid: __RECORD.recid,
+        message: message,
+      })
+      .then((resp) => {
+        console.log(resp.data);
+        setResponse({
+          data: resp.data,
+          message: (
+            <>
+              Request to{" "}
+              <a href={`/communities/${community.metadata.id}`}>
+                {community.metadata.title}
+              </a>{" "}
+              submitted
+            </>
+          ),
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setResponse({
+          response: error.response,
+          message:
+            error.response.status == 500
+              ? "Unknown error"
+              : error.response.data.message,
+          status: "error",
+        });
+      })
+      .finally(() => {
         setCommunity(null);
         setMessage("");
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    postData();
+        toggleOpen();
+      });
   };
+
+  const handleCancel = () => {
+    setCommunity(null);
+    setMessage("");
+    setResponse(null);
+    toggleOpen();
+  };
+
+  const options = data.hits.hits.map((community) => ({
+    key: community.metadata.id,
+    text: community.metadata.title,
+    value: community,
+  }));
   return (
-    <Modal
-      size="small"
-      trigger={
-        <Button onClick={() => setOpen(true)} icon>
-          <Icon name="plus" />
-        </Button>
-      }
-      closeIcon={true}
-      open={open}
-      onClose={() => setOpen(false)}
-    >
-      <Modal.Header>Select a community</Modal.Header>
-      <Modal.Content image>
-        <Modal.Description>
-          <CommunitiesSearchDropdown onSelect={handleSelect} />
+    <>
+      <Dropdown
+        text="Add community"
+        icon="add"
+        labeled
+        button
+        className="icon"
+        open={isOpen}
+        onClose={handleCancel}
+        onClick={toggleOpen}
+      >
+        <Dropdown.Menu onClick={(e) => e.stopPropagation()}>
           {community ? (
-            <Form>
-              <p>
-                Request for this record to be added to "
-                {community.metadata.title}"
-              </p>
-              <Form.Field>
-                <label>Message</label>
-                <TextArea
+            <Dropdown.Item selected={false} active={false}>
+              <Form>
+                <p>
+                  Request for this record to be added to{" "}
+                  <a href={`/communities/${community.metadata.id}`}>
+                    {community.metadata.title}
+                  </a>
+                </p>
+                <Form.Input
+                  label="Message"
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                   placeholder="Write a message for the community curator..."
                 />
-              </Form.Field>
-              <Button onClick={handleSubmit} type="submit">
-                Send request
-              </Button>
-            </Form>
-          ) : null}
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  );
-};
-
-const CommunityItem = ({ community }) => (
-  <>
-    {/* TODO: Add logos */}
-    {/* <Image src={community.logo} avatar size="tiny" /> */}
-    <span>{community.title}</span>
-  </>
-);
-
-const CommunityPendingItem = ({ community }) => {
-  const [message, setMessage] = useState("");
-
-  const handle = (action) => {
-    return () => {
-      const postData = async () => {
-        try {
-          const result = await axios.post(
-            `/api/communities/${community.id}/requests/inclusion/${community.request_id}/${action}`,
-            { message },
-          );
-          console.log(result.data);
-          setMessage("");
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      postData();
-    };
-  };
-
-  return (
-    <>
-      <CommunityItem community={community} />
-      <Icon link onClick={handle("accept")} color="green" name="check" />
-      <Icon link onClick={handle("reject")} color="red" name="x" />
-      <Icon link onClick={handle("comment")} name="comment" />
+                <Button positive onClick={handleSubmit}>
+                  Submit
+                </Button>
+                <Button negative onClick={handleCancel}>
+                  Cancel
+                </Button>
+              </Form>
+            </Dropdown.Item>
+          ) : (
+            <>
+              <Dropdown.Header content="Search communities" />
+              <Input
+                icon="search"
+                value={query}
+                // TODO: debounce
+                onChange={(e) => setQuery(e.target.value)}
+                iconPosition="left"
+                name="community-query"
+              />
+              {options.map(({ key, text, value }) => (
+                <Dropdown.Item
+                  key={key}
+                  text={text}
+                  onClick={() => setCommunity(value)}
+                />
+              ))}
+            </>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+      {/* TODO: extract error message into separate component */}
+      {response ? (
+        <Message
+          size="tiny"
+          positive={response.status !== "error"}
+          negative={response.status === "error"}
+        >
+          {response.message}
+        </Message>
+      ) : null}
     </>
   );
 };
 
-const CommunityGallery = ({ communities, renderItem }) => {
-  renderItem = renderItem || CommunityItem;
+const CommunityItem = ({ community }) => (
+  <List.Item>
+    {/* TODO: Add logos */}
+    {/* <Image src={community.logo} avatar size="tiny" /> */}
+    <List.Header as="a" href={`/communities/${community.comid}`}>
+      {community.title}
+    </List.Header>
+  </List.Item>
+);
+
+const CommunityPendingItem = ({ community }) => {
+  const [response, setResponse] = useState(null);
+
+  const handle = (action) => {
+    return () => {
+      axios
+        .post(
+          `/api/communities/${community.comid}/requests/inclusion/${community.request_id}/${action}`
+        )
+        .then((resp) => {
+          console.log(resp.data);
+          setResponse({
+            response: resp,
+            message: `Request ${action}ed`,
+            status: "success",
+          });
+        })
+        .catch((error) => {
+          console.log(error.response);
+          setResponse({
+            response: error.response,
+            message:
+              error.response.status == 500
+                ? "Unknown error"
+                : error.response.data.message,
+            status: "error",
+          });
+        });
+    };
+  };
+
   return (
-    <ul className="list-unstyled">
+    <List.Item>
+      <List.Content>
+        <List.Header as="a" href={`/communities/${community.comid}`}>
+          {community.title}
+        </List.Header>
+        <Icon link onClick={handle("accept")} color="green" name="check" />
+        <Icon link onClick={handle("reject")} color="red" name="x" />
+        <Icon link onClick={handle("comment")} name="comment" />
+        {response ? (
+          <List.Description>
+            <Message
+              size="tiny"
+              positive={response.status !== "error"}
+              negative={response.status === "error"}
+            >
+              {response.message}
+            </Message>
+          </List.Description>
+        ) : null}
+      </List.Content>
+    </List.Item>
+  );
+};
+
+const CommunityGallery = ({ communities, renderItem }) => {
+  const RenderItem = renderItem || CommunityItem;
+  return (
+    <List>
       {communities.map((community) => (
-        <li key={community.id}>{renderItem({ community })}</li>
+        <RenderItem key={community.id} community={community} />
       ))}
-    </ul>
+    </List>
   );
 };
 
 const App = () => {
+  const accepted_communities = __RECORD_COMMUNITIES.accepted || [];
+  const pending_communities = __RECORD_COMMUNITIES.pending || [];
+
   return (
     <>
-      <CommunityGallery communities={__RECORD_COMMUNITIES.accepted || []} />
+      <CommunityGallery communities={accepted_communities} />
       <Accordion
         panels={[
           {
             key: "pending-requests",
-            title: "Pending requests",
+            title: `Pending requests (${pending_communities.length})`,
             content: {
               content: (
                 <>
                   <CommunityGallery
                     renderItem={CommunityPendingItem}
-                    communities={__RECORD_COMMUNITIES.pending || []}
+                    communities={pending_communities}
                   />
-                  <CommunityRequestModal />
                 </>
               ),
             },
           },
         ]}
       />
+      <Divider />
+      <CommunitiesRequestDropdown />
     </>
   );
 };
