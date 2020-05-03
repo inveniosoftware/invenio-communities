2a229853f6d88977e8abd0f67df5c29e3359f5c4
Alexander Ioannidis <a.ioannidis@cern.ch>
2020-05-04 00:57:27 +0200

    views: clean-up community search page

--- invenio_communities/assets/js/invenio_communities/search.js
+++ invenio_communities/assets/js/invenio_communities/search.js
@@ -1,24 +1,24 @@
 import { SearchWrapper } from "./search/SearchMain";
 import ReactDOM from "react-dom";
 import React from "react";
-import { Item, Card } from 'semantic-ui-react';
-import _truncate from 'lodash/truncate';
+import { Item, Card } from "semantic-ui-react";
+import _truncate from "lodash/truncate";
 
 const aggregations = [
   {
     title: "Types",
     agg: {
       field: "type",
-      aggName: "type"
-    }
+      aggName: "type",
+    },
   },
   {
     title: "Domains",
     agg: {
       field: "domain",
-      aggName: "domain"
-    }
-  }
+      aggName: "domain",
+    },
+  },
 ];
 
 const sortValues = [
@@ -26,77 +26,86 @@ const sortValues = [
     text: "Best match",
     sortBy: "bestmatch",
     sortOrder: "desc",
-    defaultOnEmptyString: true
+    defaultOnEmptyString: true,
   },
   {
     text: "Newest",
     sortBy: "mostrecent",
     sortOrder: "asc",
-    default: true
+    default: true,
   },
   {
     text: "Oldest",
     sortBy: "mostrecent",
-    sortOrder: "desc"
-  }
+    sortOrder: "desc",
+  },
 ];
 
 const resultsPerPageValues = [
   {
     text: "10",
-    value: 10
+    value: 10,
   },
   {
     text: "20",
-    value: 20
+    value: 20,
   },
   {
     text: "50",
-    value: 50
-  }
+    value: 50,
+  },
 ];
 
-
-const searchApi = {
-  baseURL: "",
-  url: "/api/communities",
-  timeout: 5000
-};
-
-export const config = {
-  searchApi,
-  aggregations,
-  sortValues,
-  resultsPerPageValues
-};
-
-
-
-export function ResultsGridItemTemplate(record, index) {
+function ResultsGridItemTemplate(record, index) {
   return (
     <Card fluid key={index} href={`/communities/${record.metadata.id}`}>
       <Card.Content>
         <Card.Header>{record.metadata.title}</Card.Header>
         <Card.Description>
-          <div dangerouslySetInnerHTML={{ __html: record.metadata.description }} />
+          <div
+            dangerouslySetInnerHTML={{ __html: record.metadata.description }}
+          />
         </Card.Description>
       </Card.Content>
     </Card>
   );
 }
 
-
-export function ResultsItemTemplate(record, index) {
+function ResultsItemTemplate(record, index) {
   return (
     <Item key={index} href={`/communities/${record.metadata.id}`}>
       <Item.Content>
         <Item.Header>{record.metadata.title}</Item.Header>
         <Item.Description>
-          <div dangerouslySetInnerHTML={{ __html: record.metadata.description }} />
+          <div
+            dangerouslySetInnerHTML={{ __html: record.metadata.description }}
+          />
         </Item.Description>
       </Item.Content>
     </Item>
-  )
+  );
+}
+
+const searchApi = {
+  axios: {
+    baseURL: "",
+    url: "/api/communities",
+    timeout: 5000,
+  },
 };
 
-ReactDOM.render(<SearchWrapper ResultsListItem={ResultsItemTemplate} ResultsGridItem={ResultsGridItemTemplate} searchConfig={config} />, document.getElementById("communities-search"));
+const searchConfig = {
+  searchApi,
+  aggregations,
+  sortValues,
+  resultsPerPageValues,
+};
+
+ReactDOM.render(
+  <SearchWrapper
+    ResultsListItem={ResultsItemTemplate}
+    ResultsGridItem={ResultsGridItemTemplate}
+    searchConfig={searchConfig}
+  />,
+  document.getElementById("communities-search")
+);
