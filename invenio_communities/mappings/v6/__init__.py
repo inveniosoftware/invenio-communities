17059f7f10ed5da6b21c0cfa6b2e2c8c83e20656
Alexander Ioannidis <a.ioannidis@cern.ch>
2020-05-04 10:53:01 +0200

    mappings: add v6 support

---
+++ invenio_communities/mappings/v6/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2020 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Mappings for Elasticsearch 6.x."""
+
+from __future__ import absolute_import, print_function
