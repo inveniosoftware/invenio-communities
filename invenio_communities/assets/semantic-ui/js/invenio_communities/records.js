6b956e76a253e92936e9d66fda8b297df3da634f
Georgios Lignos <glignos93@gmail.com>
2020-09-07 18:00:42 +0200

    ui: minor fixes and improvements for record communities

--- invenio_communities/assets/semantic-ui/js/invenio_communities/records.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/records.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import _isEmpty from 'lodash/isEmpty';
 
 import "semantic-ui-css/semantic.min.css";
 import {
@@ -139,25 +140,25 @@ const CommunitiesRequestDropdown = () => {
               </Form>
             </Dropdown.Item>
           ) : (
-            <>
-              <Dropdown.Header content="Search communities" />
-              <Input
-                icon="search"
-                value={query}
-                // TODO: debounce
-                onChange={(e) => setQuery(e.target.value)}
-                iconPosition="left"
-                name="community-query"
-              />
-              {options.map(({ key, text, value }) => (
-                <Dropdown.Item
-                  key={key}
-                  text={text}
-                  onClick={() => setCommunity(value)}
+              <>
+                <Dropdown.Header content="Search communities" />
+                <Input
+                  icon="search"
+                  value={query}
+                  // TODO: debounce
+                  onChange={(e) => setQuery(e.target.value)}
+                  iconPosition="left"
+                  name="community-query"
                 />
-              ))}
-            </>
-          )}
+                {options.map(({ key, text, value }) => (
+                  <Dropdown.Item
+                    key={key}
+                    text={text}
+                    onClick={() => setCommunity(value)}
+                  />
+                ))}
+              </>
+            )}
         </Dropdown.Menu>
       </Dropdown>
       {/* TODO: extract error message into separate component */}
@@ -174,26 +175,33 @@ const CommunitiesRequestDropdown = () => {
   );
 };
 
-const CommunityItem = ({ community }) => (
+const CommunityItem = ({ communityRecord }) => (
   <List.Item>
     {/* TODO: Add logos */}
-    {/* <Image src={community.logo} avatar size="tiny" /> */}
-    <List.Header as="a" href={`/communities/${community.comid}`}>
-      {community.title}
+    {/* <Image src={communityRecord.logo} avatar size="tiny" /> */}
+    <List.Header as="a" href={`/communities/${communityRecord.comid}`}>
+      {communityRecord.community_title}
     </List.Header>
+    <CommunitiesCollectionDropdown communityRecord={communityRecord} />
   </List.Item>
 );
 
-const CommunityPendingItem = ({ community }) => {
+const CommunityPendingItem = ({ communityRecord }) => {
   const [response, setResponse] = useState(null);
-
+  const [showMessageInput, setShowMessageInput] = useState(false);
+  const [message, setMessage] = useState("");
+  debugger
   const handle = (action) => {
     return () => {
       axios
         .post(
-          `/api/communities/${community.comid}/records/requests/${community.request_id}/${action}`
+          `/api/communities/${communityRecord.comid}/records/requests/${communityRecord.id}/${action}`,
+          { 'message': message }
         )
         .then((resp) => {
+          if (action == 'comment') {
+            setShowMessageInput(false);
+          }
           console.log(resp.data);
           setResponse({
             response: resp,
@@ -216,36 +224,62 @@ const CommunityPendingItem = ({ community }) => {
   };
 
   return (
-    <List.Item>
-      <List.Content>
-        <List.Header as="a" href={`/communities/${community.comid}`}>
-          {community.title}
-        </List.Header>
-        <Icon link onClick={handle("accept")} color="green" name="check" />
-        <Icon link onClick={handle("reject")} color="red" name="x" />
-        <Icon link onClick={handle("comment")} name="comment" />
-        {response ? (
-          <List.Description>
-            <Message
-              size="tiny"
-              positive={response.status !== "error"}
-              negative={response.status === "error"}
-            >
-              {response.message}
-            </Message>
-          </List.Description>
-        ) : null}
-      </List.Content>
-    </List.Item>
+    <>
+      <List.Item>
+        <List.Content>
+          <List.Header as="a" href={`/communities/${communityRecord.comid}`}>
+            {communityRecord.community_title}
+          </List.Header>
+          <Icon link onClick={handle("accept")} color="green" name="check" />
+          <Icon link onClick={handle("reject")} color="red" name="x" />
+          <Icon link onClick={() => setShowMessageInput(true)} name="comment" />
+          {response ? (
+            <List.Description>
+              <Message
+                size="tiny"
+                positive={response.status !== "error"}
+                negative={response.status === "error"}
+              >
+                {response.message}
+              </Message>
+            </List.Description>
+          ) : null}
+        </List.Content>
+      </List.Item>
+      {showMessageInput ?
+        <div className="ui active modal">
+          <Form>
+            <div className="content">
+              <p>
+                Send a message to the other party.
+          </p>
+              <Form.Input
+                label="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Write a message for the community curator..."
+              />
+            </div>
+            <div className="actions">
+              <Button positive onClick={handle("comment")}>
+                Submit
+          </Button>
+              <Button negative onClick={() => setShowMessageInput(false)}>
+                Cancel
+          </Button>
+            </div>
+          </Form>
+        </div> : null}
+    </>
   );
 };
 
-const CommunityGallery = ({ communities, renderItem }) => {
+const CommunityGallery = ({ communitiesRecords, renderItem }) => {
   const RenderItem = renderItem || CommunityItem;
   return (
     <List>
-      {communities.map((community) => (
-        <RenderItem key={community.id} community={community} />
+      {communitiesRecords.map((communityRecord) => (
+        <RenderItem key={communityRecord.id} communityRecord={communityRecord} />
       ))}
     </List>
   );
@@ -254,10 +288,9 @@ const CommunityGallery = ({ communities, renderItem }) => {
 const App = () => {
   const accepted_communities = __RECORD_COMMUNITIES.accepted || [];
   const pending_communities = __RECORD_COMMUNITIES.pending || [];
-
   return (
     <>
-      <CommunityGallery communities={accepted_communities} />
+      <CommunityGallery communitiesRecords={accepted_communities} />
       <Accordion
         panels={[
           {
@@ -268,7 +301,7 @@ const App = () => {
                 <>
                   <CommunityGallery
                     renderItem={CommunityPendingItem}
-                    communities={pending_communities}
+                    communitiesRecords={pending_communities}
                   />
                 </>
               ),
@@ -282,7 +315,110 @@ const App = () => {
   );
 };
 
+const CommunitiesCollectionDropdown = ({ communityRecord }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [ongoingRequest, setOngoingRequest] = useState(false);
+  const [response, setResponse] = useState(null);
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const recordCollections= communityRecord._collections || []
+  const [currentCollections, setCurrentCollections] = useState(recordCollections.map((collection)=>
+  collection.id
+));
+
+
+  const handleChange = (new_collections) => {
+    setOngoingRequest(true);
+    axios
+      .put(`/api/communities/${communityRecord.comid}/records/${__RECORD.recid}`, {
+        'collections': new_collections.map(collection => ({
+          'id': collection
+        }))
+      })
+      .then((resp) => {
+        setCurrentCollections(new_collections);
+        setResponse({
+          // TODO
+          data: resp.data,
+          message: (
+            <>
+              The collections for this record have been updated.
+            </>
+          ),
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setResponse({
+          response: error.response,
+          message:
+            error.response.status == 500
+              ? "Unknown error"
+              : error.response.data.message,
+          status: "error",
+        });
+      })
+      .finally(() => {
+        setOngoingRequest(false);
+      });
+  };
+
+  const availableCollections = __COMMUNITIES[communityRecord.comid]._collections || {}
+
+  const options = Object.entries(availableCollections).map(([collection_id, collection]) => ({
+    key: collection_id,
+    text: collection.title,
+    value: collection_id,
+  }));
+
+
+console.log(availableCollections)
+  return (
+    (!_isEmpty(options) ?
+      ( toggleEdit ? (
+      <>
+      <Dropdown
+        placeholder='collections'
+        fluid
+        multiple selection
+        disabled={ongoingRequest}
+        options={options}
+        defaultValue={currentCollections}
+        onChange={(e, { value }) => handleChange(value)} />
+        { response ? (
+            <Message
+              size="tiny"
+              positive={response.status !== "error"}
+              negative={response.status === "error"}
+            >
+              {response.message}
+            </Message>
+          ) : null
+        }
+        <Button positive size="small" onClick={() => setToggleEdit(false)}>Done</Button>
+      </>):
+      <>
+      <List>
+        {currentCollections.map((collection_id)=>(
+          <List.Item key={collection_id}>
+            <List.Header>
+              {availableCollections[collection_id].title}
+            </List.Header>
+          </List.Item>
+        ))}
+      </List>
+      <Button color="orange" size="small" onClick={() => setToggleEdit(true)}>Edit</Button>
+      </>
+      )
+      : null)
+  );
+};
+
+
 const domContainer = document.querySelector("#communities-management");
 const __RECORD = JSON.parse(domContainer.dataset.record);
-const __RECORD_COMMUNITIES = JSON.parse(domContainer.dataset.communities);
+const __COMMUNITIES_DATA = JSON.parse(domContainer.dataset.communities)
+const __RECORD_COMMUNITIES = __COMMUNITIES_DATA.record_communities;
+const __COMMUNITIES = __COMMUNITIES_DATA.communities;
+
 ReactDOM.render(<App />, domContainer);
