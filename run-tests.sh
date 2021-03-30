a30548f2c46e435279460c40a14c3c2ea00b20bc
Alexander Ioannidis <a.ioannidis@cern.ch>
2021-03-30 17:25:12 +0200

    global: add GH actions and clean-up

--- run-tests.sh
+++ run-tests.sh
@@ -1,20 +1,57 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2016-2020 CERN.
+# Copyright (C) 2021 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-isort invenio_communities tests --check-only --diff && \
-check-manifest --ignore ".travis-*" && \
-pytest
+# Usage:
+#   ./run-tests.sh [-K|--keep-services] [pytest options and args...]
+#
+# Note: the DB, SEARCH and MQ services to use are determined by corresponding environment
+#       variables if they are set -- otherwise, the following defaults are used:
+#       DB=postgresql, SEARCH=elasticsearch and MQ=redis
+#
+# Example for using mysql instead of postgresql:
+#    DB=mysql ./run-tests.sh
 
-# TODO: Enable when tests are in place
-# pydocstyle invenio_communities tests && \
-# isort -rc -c -df && \
-# check-manifest --ignore ".travis-*" && \
-# sphinx-build -qnNW docs docs/_build/html && \
-# pytest && \
-# sphinx-build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+# Define function for bringing down services
+function cleanup {
+  eval "$(docker-services-cli down --env)"
+}
+
+# Check for arguments
+# Note: "-k" would clash with "pytest"
+keep_services=0
+pytest_args=()
+for arg in $@; do
+	# from the CLI args, filter out some known values and forward the rest to "pytest"
+	# note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+	#       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+	case ${arg} in
+		-K|--keep-services)
+			keep_services=1
+			;;
+		*)
+			pytest_args+=( ${arg} )
+			;;
+	esac
+done
+
+if [[ ${keep_services} -eq 0 ]]; then
+	trap cleanup EXIT
+fi
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --mq ${MQ:-redis} --env)"
+python -m pytest "${pytest_args[@]}"
+tests_exit_code=$?
+exit "$tests_exit_code"
