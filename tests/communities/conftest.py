edb11d816a488bdfbcf0aa4c87b33d757cc95650
effervescent-shot <nhlezgiyctrk@gmail.com>
2021-04-15 11:45:50 +0200

    tests: initial test run in remote

--- tests/communities/conftest.py
+++ tests/communities/conftest.py
@@ -12,7 +12,7 @@ import uuid
 
 import pytest
 from flask_security import login_user
-from invenio_accounts.testutils import create_test_user, login_user_via_session
+from  .testutils import create_test_user, login_user_via_session
 from invenio_app.factory import create_api
 
 
