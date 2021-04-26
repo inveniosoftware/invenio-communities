50620c694ba1d984afd10d0818e8d5ff0633fd67
Alexander Ioannidis <a.ioannidis@cern.ch>
2021-04-26 15:36:23 +0200

    access: add missing member/record policy

--- invenio_communities/communities/records/systemfields/access.py
+++ invenio_communities/communities/records/systemfields/access.py
@@ -35,12 +35,16 @@ class CommunityAccess:
     """Access management per community."""
 
     VISIBILITY_LEVELS = ('public', 'restricted')
+    MEMBER_POLICY_LEVELS = ('open', 'closed')
+    RECORD_POLICY_LEVELS = ('open', 'closed', 'restricted')
 
     owners_cls = _Owners
 
     def __init__(
         self,
         visibility=None,
+        member_policy=None,
+        record_policy=None,
         owned_by=None,
         owners_cls=None,
     ):
@@ -53,6 +57,8 @@ class CommunityAccess:
         :param owned_by: The set of community owners
         """
         self.visibility = visibility or 'public'
+        self.member_policy = member_policy or 'open'
+        self.record_policy = record_policy or 'open'
 
         owners_cls = owners_cls or self.owners_cls
         self.owned_by = owned_by if owned_by else owners_cls()
@@ -61,6 +67,12 @@ class CommunityAccess:
 
     def _validate_visibility_level(self, level):
         return level in self.VISIBILITY_LEVELS
+
+    def _validate_member_policy_level(self, level):
+        return level in self.MEMBER_POLICY_LEVELS
+
+    def _validate_record_policy_level(self, level):
+        return level in self.RECORD_POLICY_LEVELS
 
     @property
     def visibility(self):
@@ -74,10 +86,36 @@ class CommunityAccess:
             raise ValueError(f"Unknown visibility level: {value}")
         self._visibility = value
 
+    @property
+    def member_policy(self):
+        """Get the member policy level."""
+        return self._member_policy
+
+    @member_policy.setter
+    def member_policy(self, value):
+        """Set the member policy level."""
+        if not self._validate_member_policy_level(value):
+            raise ValueError(f"Unknown member policy level: {value}")
+        self._member_policy = value
+
+    @property
+    def record_policy(self):
+        """Get the record policy level."""
+        return self._record_policy
+
+    @record_policy.setter
+    def record_policy(self, value):
+        """Set the record policy level."""
+        if not self._validate_record_policy_level(value):
+            raise ValueError(f"Unknown record policy level: {value}")
+        self._record_policy = value
+
     def dump(self):
         """Dump the field values as dictionary."""
         return {
             "visibility": self.visibility,
+            "member_policy": self.member_policy,
+            "record_policy": self.record_policy,
             "owned_by": self.owned_by.dump(),
         }
 
@@ -85,6 +123,8 @@ class CommunityAccess:
         """Re-initialize the Access object with the data in the access_dict."""
         new_access = self.from_dict(access_dict)
         self.visibility = new_access.visibility
+        self.member_policy = new_access.member_policy
+        self.record_policy = new_access.record_policy
         self.owned_by = new_access.owned_by
 
     @classmethod
@@ -114,15 +154,27 @@ class CommunityAccess:
                 except Exception as e:
                     errors.append(e)
 
-        access = cls(visibility=access_dict["visibility"], owned_by=owned_by)
+        access = cls(
+            visibility=access_dict.get("visibility"),
+            member_policy=access_dict.get("member_policy"),
+            record_policy=access_dict.get("record_policy"),
+            owned_by=owned_by
+        )
         access.errors = errors
         return access
 
     def __repr__(self):
         """Return repr(self)."""
-        return "<{} (visibility: {}, owners: {})>".format(
+        return (
+            "<{} (visibility: {}, "
+            "member_policy: {}, "
+            "record_policy: {}, "
+            "owners: {})>"
+        ).format(
             type(self).__name__,
             self.visibility,
+            self.member_policy,
+            self.record_policy,
             self.owned_by,
         )
 
