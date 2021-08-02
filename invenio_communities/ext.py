9df718b41f92fba06c45214ef704b095cd6daeac
Pablo Panero <pablo.panero@cern.ch>
2021-08-02 17:34:42 +0200

    ext: do not register routes and service if disabled

--- invenio_communities/ext.py
+++ invenio_communities/ext.py
@@ -30,8 +30,9 @@ class InvenioCommunities(object):
         self.init_config(app)
         app.extensions['invenio-communities'] = self
 
-        self.init_services(app)
-        self.init_resource(app)
+        if app.config["COMMUNITIES_ENABLED"]:
+            self.init_services(app)
+            self.init_resource(app)
 
     def init_config(self, app):
         """Initialize configuration.
