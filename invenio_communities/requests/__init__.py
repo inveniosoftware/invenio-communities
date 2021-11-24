fd622368745f6d1c7958cb7f4c935bbe6944142b
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-11-24 10:18:24 +0100

    requests: entity resolver for communities

---
+++ invenio_communities/requests/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Communities is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Integration with the Invenio-Requests module."""
+
+from .resolver import CommunityResolver
+
+__all__ = (
+    'CommunityResolver',
+)
