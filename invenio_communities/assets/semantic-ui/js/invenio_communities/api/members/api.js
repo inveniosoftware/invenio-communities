64b426302a2d28ebee4e2beb272b25a551ad74c3
Pablo García Marcos <pineirin@gmail.com>
2022-05-06 12:09:17 +0200

    Members: update data modified by the bulk actions (#602)
    * closes: #556

--- invenio_communities/assets/semantic-ui/js/invenio_communities/api/members/api.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/api/members/api.js
@@ -4,9 +4,9 @@
 // Invenio-communities is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import { bulkMembersSerializer } from "../serializers";
 import { CommunityLinksExtractor } from "../CommunityLinksExtractor";
 import { http } from "../config";
+import { bulkMembersSerializer } from "../serializers";
 
 export class CommunityMembersApi {
   #urls;
@@ -43,32 +43,37 @@ export class CommunityMembersApi {
     return this.removeMemberBase({ members: memberSerialized });
   };
 
-  updateMembers = async (payload) => {
-    return await http.put(this.endpoint, payload);
+  #updateMembers = async (payload, { refresh = false } = {}) => {
+    const url = new URL(this.endpoint);
+    const params = url.searchParams;
+    params.delete("refresh");
+    refresh && params.set("refresh", refresh);
+    url.search = params.toString();
+    return await http.put(url.toString(), payload);
   };
 
   updateRole = async (member, role) => {
     const memberSerialized = bulkMembersSerializer([member]);
     const payload = { members: memberSerialized, role: role };
-    return await this.updateMembers(payload);
+    return await this.#updateMembers(payload);
   };
 
   updateVisibility = async (member, visibility) => {
     const memberSerialized = bulkMembersSerializer([member]);
     const payload = { members: memberSerialized, visible: visibility };
-    return await this.updateMembers(payload);
+    return await this.#updateMembers(payload);
   };
 
   bulkUpdateRoles = (members, role) => {
     const membersSerialized = bulkMembersSerializer(members);
     const payload = { members: membersSerialized, role: role };
-    return this.updateMembers(payload);
+    return this.#updateMembers(payload, { refresh: true });
   };
 
   bulkUpdateVisibilities = (members, visibility) => {
     const membersSerialized = bulkMembersSerializer(members);
 
     const payload = { members: membersSerialized, visible: visibility };
-    return this.updateMembers(payload);
+    return this.#updateMembers(payload, { refresh: true });
   };
 }
