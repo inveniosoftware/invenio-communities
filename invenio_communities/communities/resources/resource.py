c7cf907344c26a3cccaea3c5ca04eda956b8a604
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-05-12 17:27:21 +0200

    resources: add search schema for community requests

--- invenio_communities/communities/resources/resource.py
+++ invenio_communities/communities/resources/resource.py
@@ -11,13 +11,18 @@
 """Invenio Communities Resource API."""
 
 from flask import g
-from flask_resources import resource_requestctx, response_handler, route
+from flask_resources import from_conf, request_parser, resource_requestctx, \
+    response_handler, route
 from invenio_records_resources.resources.files.resource import request_stream
 from invenio_records_resources.resources.records.resource import \
     RecordResource, request_data, request_headers, request_search_args, \
     request_view_args
 from invenio_records_resources.resources.records.utils import es_preference
 
+
+request_community_requests_search_args = request_parser(
+    from_conf('request_community_requests_search_args'), location='args'
+)
 
 class CommunityResource(RecordResource):
     """Communities resource."""
@@ -128,7 +133,7 @@ class CommunityResource(RecordResource):
         return hits.to_dict(), 200
 
     @request_view_args
-    @request_search_args
+    @request_community_requests_search_args
     @response_handler(many=True)
     def search_community_requests(self):
         """Perform a search over the community's requests.
