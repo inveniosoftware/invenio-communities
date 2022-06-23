24552f279b4003620f59b431ca64a42982e7d644
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-06-23 17:47:08 +0200

    release: v2.8.4

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "2.8.3"
+__version__ = "2.8.4"
 
 __all__ = ("InvenioCommunities", "current_communities")
