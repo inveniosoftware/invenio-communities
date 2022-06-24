8d442d288fedba968ee15d17c6f46c679199bdac
Mojib Wali <44528277+mb-wali@users.noreply.github.com>
2022-06-24 09:18:27 +0200

    release: v2.8.5

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "2.8.4"
+__version__ = "2.8.5"
 
 __all__ = ("InvenioCommunities", "current_communities")
