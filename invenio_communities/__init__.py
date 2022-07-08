1ba584fbc3ed81e1746112de6efb7071705fe426
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-07-08 14:10:14 +0200

    release: v2.8.7

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "2.8.6"
+__version__ = "2.8.7"
 
 __all__ = ("InvenioCommunities", "current_communities")
