4391d7bbcc94193e31c0cf591116e79c75023615
Pablo Panero <pablo.panero@cern.ch>
2022-08-23 11:04:52 +0200

    release: v2.9.0.dev1

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "2.9.0.dev0"
+__version__ = "2.9.0.dev1"
 
 __all__ = ("InvenioCommunities", "current_communities")
