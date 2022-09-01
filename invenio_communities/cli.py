f2b32e56761b22f3be2bb5b9c30ae6995ceca35a
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-09-01 15:16:31 +0200

    custom-fields: add config and cli

--- invenio_communities/cli.py
+++ invenio_communities/cli.py
@@ -9,7 +9,9 @@
 """Command-line tools for demo module."""
 
 import click
+from elasticsearch.exceptions import RequestError
 from faker import Faker
+from flask import current_app
 from flask.cli import with_appcontext
 from invenio_access.permissions import system_identity
 
@@ -47,3 +49,106 @@ def rebuild_index():
     communities_service.rebuild_index(identity=system_identity)
 
     click.secho("Reindexed communities!", fg="green")
+
+
+@communities.group()
+def custom_fields():
+    """Communities custom fields commands."""
+
+
+# helper functions
+def _prepare_mapping(fields_name, available_fields):
+    """Prepare ES mapping properties for each field."""
+    available_fields = {field.name: field for field in available_fields}
+    fields = []
+    if fields_name:  # create only specified fields
+        for field_name in fields_name:
+            if field_name in available_fields.keys():
+                fields.append(available_fields[field_name])
+            else:
+                click.secho(
+                    "Field {0} not found. Available fields: {1}".format(
+                        field_name, ", ".join(available_fields.keys())
+                    ),
+                    fg="red",
+                )
+    else:  # create all fields
+        fields = available_fields.values()
+
+    properties = {}
+    for field in fields:
+        properties[f"custom_fields.{field.name}"] = field.mapping
+
+    return properties
+
+
+def _exists(field_name, index):
+    """Check if a field exists in `index`'s mapping."""
+    mapping = list(index.get_mapping().values())[0]["mappings"]
+
+    parts = field_name.split(".")
+    for part in parts:
+        mapping = mapping["properties"]  # here to avoid last field access
+        if part not in mapping.keys():
+            return False
+        mapping = mapping[part]
+
+    return True
+
+
+@custom_fields.command("init")
+@click.option(
+    "-f",
+    "--field-name",
+    type=str,
+    required=False,
+    multiple=True,
+    help="A custom field name to create. If not provided, all custom fields will be created.",
+)
+@with_appcontext
+def create_communities_custom_field(field_name):
+    """Creates one or all custom fields for communities.
+
+    $ invenio custom-fields communities create [field].
+    """
+    available_fields = current_app.config.get("COMMUNITIES_CUSTOM_FIELDS")
+    if not available_fields:
+        click.secho("No custom fields were configured. Exiting...", fg="green")
+        return
+
+    click.secho("Creating communities custom fields...", fg="green")
+    # multiple=True makes it an iterable
+    properties = _prepare_mapping(field_name, available_fields)
+
+    try:
+        communities_index = current_communities.service.config.record_cls.index
+        communities_index.put_mapping(body={"properties": properties})
+        click.secho("Created all communities custom fields!", fg="green")
+    except RequestError as e:
+        click.secho("An error occured while creating custom fields.", fg="red")
+        click.secho(e.info["error"]["reason"], fg="red")
+
+
+@custom_fields.command("exists")
+@click.option(
+    "-f",
+    "--field-name",
+    type=str,
+    required=True,
+    multiple=False,
+    help="A custom field name to check.",
+)
+@with_appcontext
+def custom_field_exists_in_communities(field_name):
+    """Checks if a custom field exists in communities ES mapping.
+
+    $ invenio custom-fields communities exists <field name>.
+    """
+    click.secho("Checking custom field...", fg="green")
+    communities_index = current_communities.service.config.record_cls.index
+
+    field_exists = _exists(f"custom_fields.{field_name}", communities_index)
+    if field_exists:
+        click.secho(f"Field {field_name} exists", fg="green")
+    else:
+        click.secho(f"Field {field_name} does not exist", fg="red")
--- tests/communities/test_cli.py
+++ tests/communities/test_cli.py
@@ -10,6 +10,11 @@
 
 from faker import Faker
 
+from invenio_communities.cli import (
+    create_communities_custom_field,
+    custom_field_exists_in_communities,
+)
+from invenio_communities.communities.records.api import Community
 from invenio_communities.fixtures.demo import create_fake_community
 from invenio_communities.fixtures.tasks import create_demo_community
 
@@ -20,3 +25,31 @@ def test_fake_demo_community_creation(
     """Assert that demo community creation works without failing."""
     faker = Faker()
     create_demo_community(create_fake_community(faker))
+
+
+def test_create_communities_custom_fields(app, location, db, es_clear, cli_runner):
+    """Assert that custom fields mappings are created for communities."""
+    result = cli_runner(create_communities_custom_field, "-f", "mycommunityfield")
+    assert result.exit_code == 0
+
+    community_mapping_field = list(Community.index.get_mapping().values())[0][
+        "mappings"
+    ]["properties"]["custom_fields"]
+    expected_value = {
+        "properties": {
+            "mycommunityfield": {
+                "type": "text",
+                "fields": {"keyword": {"type": "keyword"}},
+            }
+        }
+    }
+    assert community_mapping_field == expected_value
+
+    # check for existence
+    result = cli_runner(custom_field_exists_in_communities, "-f", "mycommunityfield")
+    assert result.exit_code == 0
+    assert "Field mycommunityfield exists" in result.output
+
+    result = cli_runner(custom_field_exists_in_communities, "-f", "unknownfield")
+    assert result.exit_code == 0
+    assert "Field unknownfield does not exist" in result.output
