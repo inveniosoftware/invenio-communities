9c1a3ebde29be30fff9d68194def95f1c1818ee7
pablo <pineirin@gmail.com>
2022-09-07 17:35:18 +0200

    Facets: move facets to invenio-search-ui
    * closes: inveniosoftware/invenio-administration#56

--- invenio_communities/assets/semantic-ui/js/invenio_communities/requests/requests.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/requests/requests.js
@@ -12,11 +12,9 @@ import {
 import { i18next } from "@translations/invenio_communities/i18next";
 import { DateTime } from "luxon";
 import PropTypes from "prop-types";
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { GridResponsiveSidebarColumn } from "react-invenio-forms";
-import Overridable from "react-overridable";
 import {
-  BucketAggregation,
   Count,
   ResultsList,
   SearchBar,
@@ -24,17 +22,13 @@ import {
   withState,
 } from "react-searchkit";
 import {
-  Accordion,
   Button,
   Card,
-  Checkbox,
   Container,
   Grid,
   Header,
   Icon,
   Input,
-  Label,
-  List,
   Segment,
 } from "semantic-ui-react";
 import { ComputerTabletRequestsItem } from "./requests_items/ComputerTabletRequestsItem";
@@ -84,158 +78,6 @@ export const RecordSearchBarElement = withState(
     );
   }
 );
-
-export const ParentFacetValue = ({
-  bucket,
-  keyField,
-  isSelected,
-  childAggCmps,
-  onFilterClicked,
-}) => {
-  const [isActive, setIsActive] = useState(false);
-
-  return (
-    <Accordion className="rdm-multi-facet">
-      <Accordion.Title
-        onClick={() => {}}
-        key={`panel-${bucket.label}`}
-        active={isActive}
-        className="facet-wrapper parent"
-      >
-        <List.Content className="facet-wrapper">
-          <Button
-            icon="angle right"
-            className="transparent"
-            onClick={() => setIsActive(!isActive)}
-            aria-label={
-              i18next.t("Show all sub facets of ") + bucket.label || keyField
-            }
-          />
-          <Checkbox
-            label={bucket.label || keyField}
-            id={`${keyField}-facet-checkbox`}
-            aria-describedby={`${keyField}-count`}
-            value={keyField}
-            checked={isSelected}
-            onClick={() => onFilterClicked(keyField)}
-          />
-          <Label circular id={`${keyField}-count`} className="facet-count">
-            {bucket.doc_count}
-          </Label>
-        </List.Content>
-      </Accordion.Title>
-      <Accordion.Content active={isActive}>{childAggCmps}</Accordion.Content>
-    </Accordion>
-  );
-};
-
-export const FacetValue = ({
-  bucket,
-  keyField,
-  isSelected,
-  onFilterClicked,
-}) => {
-  return (
-    <>
-      <List.Content className="facet-wrapper">
-        <Checkbox
-          onClick={() => onFilterClicked(keyField)}
-          label={bucket.label || keyField}
-          id={`${keyField}-facet-checkbox`}
-          aria-describedby={`${keyField}-count`}
-          value={keyField}
-          checked={isSelected}
-        />
-        <Label circular id={`${keyField}-count`} className="facet-count">
-          {bucket.doc_count}
-        </Label>
-      </List.Content>
-    </>
-  );
-};
-
-export const RecordFacetsValues = ({
-  bucket,
-  isSelected,
-  onFilterClicked,
-  childAggCmps,
-}) => {
-  const hasChildren = childAggCmps && childAggCmps.props.buckets.length > 0;
-  const keyField = bucket.key_as_string ? bucket.key_as_string : bucket.key;
-  return (
-    <List.Item key={bucket.key}>
-      {hasChildren ? (
-        <ParentFacetValue
-          bucket={bucket}
-          keyField={keyField}
-          isSelected={isSelected}
-          childAggCmps={childAggCmps}
-          onFilterClicked={onFilterClicked}
-        />
-      ) : (
-        <FacetValue
-          bucket={bucket}
-          keyField={keyField}
-          isSelected={isSelected}
-          onFilterClicked={onFilterClicked}
-        />
-      )}
-    </List.Item>
-  );
-};
-
-export const BucketAggregationElement = ({
-  agg,
-  title,
-  containerCmp,
-  updateQueryFilters,
-}) => {
-  const clearFacets = () => {
-    if (containerCmp.props.selectedFilters.length) {
-      updateQueryFilters([agg.aggName, ""], containerCmp.props.selectedFilters);
-    }
-  };
-
-  const hasSelections = () => {
-    return !!containerCmp.props.selectedFilters.length;
-  };
-
-  return (
-    <Card className="borderless facet">
-      <Card.Content>
-        <Card.Header as="h2">
-          {title}
-          {hasSelections() && (
-            <Button
-              basic
-              icon
-              size="mini"
-              floated="right"
-              onClick={clearFacets}
-              aria-label={i18next.t("Clear selection")}
-              title={i18next.t("Clear selection")}
-            >
-              {i18next.t("Clear")}
-            </Button>
-          )}
-        </Card.Header>
-        {containerCmp}
-      </Card.Content>
-    </Card>
-  );
-};
-
-const SearchHelpLinks = () => {
-  return (
-    <Overridable id={"Search.SearchHelpLinks"}>
-      <List>
-        <List.Item>
-          <a href="/help/search">{i18next.t("Search guide")}</a>
-        </List.Item>
-      </List>
-    </Overridable>
-  );
-};
 
 export const RequestsResults = ({
   sortOptions,
@@ -510,27 +352,6 @@ export const RequestsSearchLayout = (props) => {
         </Grid.Row>
       </Grid>
     </Container>
-  );
-};
-
-export const RequestsFacets = ({ aggs, currentResultsState }) => {
-  return (
-    <aside aria-label={i18next.t("filters")} id="search-filters">
-      {aggs.map((agg) => {
-        return (
-          <div className="rdm-facet-container" key={agg.title}>
-            <BucketAggregation title={agg.title} agg={agg} />
-          </div>
-        );
-      })}
-
-      <Card className="borderless facet mt-0">
-        <Card.Content>
-          <Card.Header as="h2">{i18next.t("Help")}</Card.Header>
-          <SearchHelpLinks />
-        </Card.Content>
-      </Card>
-    </aside>
   );
 };
 
