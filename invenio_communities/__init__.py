e919e84904509f67155dd52d164244fa52108174
Nicola Tarocco <ntarocco@gmail.com>
2022-09-28 15:51:13 +0200

    release: v3.0.1

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 __all__ = ("InvenioCommunities", "current_communities")
