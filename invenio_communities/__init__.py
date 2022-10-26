9e3eaf36e3bd4a3d448269cc02ff68f5b0379601
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-10-26 16:33:41 +0200

    release: v3.2.2

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "3.2.1"
+__version__ = "3.2.2"
 
 __all__ = ("InvenioCommunities", "current_communities")
