1b09ac13a97b5312eeacdbfc2ea371f0fcd3c4c4
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-11-02 13:17:15 +0100

    permissions: disable write operations if read-only mode is on

--- invenio_communities/permissions.py
+++ invenio_communities/permissions.py
@@ -2,9 +2,9 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2022 CERN.
-# Copyright (C) 2021 Graz University of Technology.
-# Copyright (C) 2021 TU Wien.
-# Copyright (C) 2022 Northwestern University.
+# Copyright (C) 2021      Graz University of Technology.
+# Copyright (C) 2021-2022 TU Wien.
+# Copyright (C)      2022 Northwestern University.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -15,6 +15,7 @@ from invenio_records_permissions.generators import (
     AnyUser,
     AuthenticatedUser,
     Disable,
+    DisableIfReadOnly,
     SystemProcess,
 )
 from invenio_records_permissions.policies import BasePermissionPolicy
@@ -38,16 +39,16 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
     """Permissions for Community CRUD operations."""
 
     # Community
-    can_create = [AuthenticatedUser(), SystemProcess()]
+    can_create = [AuthenticatedUser(), SystemProcess(), DisableIfReadOnly()]
 
     can_read = [
         IfRestricted("visibility", then_=[CommunityMembers()], else_=[AnyUser()]),
         SystemProcess(),
     ]
 
-    can_update = [CommunityOwners(), SystemProcess()]
+    can_update = [CommunityOwners(), SystemProcess(), DisableIfReadOnly()]
 
-    can_delete = [CommunityOwners(), SystemProcess()]
+    can_delete = [CommunityOwners(), SystemProcess(), DisableIfReadOnly()]
 
     can_search = [AnyUser(), SystemProcess()]
 
@@ -57,7 +58,7 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
 
     can_search_requests = [CommunityManagers(), CommunityCurators(), SystemProcess()]
 
-    can_rename = [CommunityOwners(), SystemProcess()]
+    can_rename = [CommunityOwners(), SystemProcess(), DisableIfReadOnly()]
 
     can_submit_record = [
         IfPolicyClosed(
@@ -71,6 +72,7 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
                 ),
             ],
         ),
+        DisableIfReadOnly(),
     ]
 
     can_members_add = [
@@ -78,17 +80,20 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
         AllowedMemberTypes("group"),
         GroupsEnabled("group"),
         SystemProcess(),
+        DisableIfReadOnly(),
     ]
 
     can_members_invite = [
         CommunityManagersForRole(),
         AllowedMemberTypes("user", "email"),
         SystemProcess(),
+        DisableIfReadOnly(),
     ]
 
     can_members_manage = [
         CommunityManagers(),
         SystemProcess(),
+        DisableIfReadOnly(),
     ]
 
     can_members_search = [
@@ -105,6 +110,7 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
     can_members_bulk_update = [
         CommunityMembers(),
         SystemProcess(),
+        DisableIfReadOnly(),
     ]
 
     can_members_bulk_delete = can_members_bulk_update
@@ -114,16 +120,17 @@ class CommunityPermissionPolicy(BasePermissionPolicy):
         CommunityManagersForRole(),
         CommunitySelfMember(),
         SystemProcess(),
+        DisableIfReadOnly(),
     ]
 
     # Ability to delete a single membership
     can_members_delete = can_members_update
 
-    can_invite_owners = [CommunityOwners(), SystemProcess()]
+    can_invite_owners = [CommunityOwners(), SystemProcess(), DisableIfReadOnly()]
 
     # Abilities for featured communities
     can_featured_search = [AnyUser(), SystemProcess()]
     can_featured_list = [SystemProcess()]
-    can_featured_create = [SystemProcess()]
-    can_featured_update = [SystemProcess()]
-    can_featured_delete = [SystemProcess()]
+    can_featured_create = [SystemProcess(), DisableIfReadOnly()]
+    can_featured_update = [SystemProcess(), DisableIfReadOnly()]
+    can_featured_delete = [SystemProcess(), DisableIfReadOnly()]
