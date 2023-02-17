450b41a9218bcadaa0dc358dbcc342ef82f795ef
Javier Romero Castro <jrcastro9515@gmail.com>
2023-02-17 11:29:01 +0100

    services: add configurable components
    * closes https://github.com/inveniosoftware/invenio-app-rdm/issues/2092

--- invenio_communities/communities/__init__.py
+++ invenio_communities/communities/__init__.py
@@ -14,6 +14,7 @@ from .services import (
     CommunityFileServiceConfig,
     CommunityService,
     CommunityServiceConfig,
+    DefaultCommunityComponents,
     SearchOptions,
 )
 
@@ -24,4 +25,5 @@ __all__ = (
     "CommunityResource",
     "CommunityResourceConfig",
     "SearchOptions",
+    "DefaultCommunityComponents",
 )
