76d7fbb796f27f10a4028e8433a6536ef9541ce4
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-03-27 10:15:30 +0200

    assets: refactor requests components

--- invenio_communities/assets/semantic-ui/js/invenio_communities/requests/index.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/requests/index.js
@@ -7,26 +7,8 @@
  */
 
 import {
-  RequestAcceptButton,
-  RequestCancelButton,
-  RequestDeclineButton,
-} from "@js/invenio_requests/components/Buttons";
-import {
-  RequestAcceptModalTrigger,
-  RequestCancelModalTrigger,
-  RequestDeclineModalTrigger,
-} from "@js/invenio_requests/components/ModalTriggers";
-import {
-  LabelStatusAccept,
-  LabelStatusCancel,
-  LabelStatusDecline,
-  LabelStatusDelete,
-  LabelStatusExpire,
-  LabelStatusSubmit,
-  LabelTypeCommunityInclusion,
-  LabelTypeCommunityInvitation,
-  LabelTypeCommunitySubmission,
-} from "@js/invenio_requests/request";
+  defaultContribComponents
+} from "@js/invenio_requests/contrib";
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import {
   ContribBucketAggregationElement,
@@ -37,12 +19,14 @@ import React from "react";
 import { overrideStore, parametrize } from "react-overridable";
 import {
   RecordSearchBarElement,
+  RequestsResultsItemTemplateCommunity,
+} from "./requests";
+import {
+  RequestsResultsGridItemTemplate,
+  RequestsSearchLayout,
   RequestsEmptyResultsWithState,
   RequestsResults,
-  RequestsResultsGridItemTemplate,
-  RequestsResultsItemTemplateCommunity,
-  RequestsSearchLayout,
-} from "./requests";
+} from "@js/invenio_requests/search";
 
 const domContainer = document.getElementById("communities-request-search-root");
 
@@ -63,48 +47,11 @@ const RequestsResultsItemTemplateWithCommunity = parametrize(
     community: community,
   }
 );
-const RequestAcceptModalTriggerWithConfig = parametrize(RequestAcceptModalTrigger, {
-  size: "mini",
-  className: "ml-5",
-});
-
-const RequestDeclineModalTriggerWithConfig = parametrize(RequestDeclineModalTrigger, {
-  size: "mini",
-  className: "ml-5",
-});
-
-const RequestCancelModalTriggerWithConfig = parametrize(RequestCancelModalTrigger, {
-  size: "mini",
-  className: "ml-5",
-});
 
 const RequestsSearchLayoutWAppName = parametrize(RequestsSearchLayout, {
   appName: appName,
 });
 
-const CommunitySubmission = () => (
-  <LabelTypeCommunitySubmission className="primary" size="small" />
-);
-
-const CommunityInclusion = () => (
-  <LabelTypeCommunityInclusion className="primary" size="small" />
-);
-
-const CommunityInvitation = () => (
-  <LabelTypeCommunityInvitation className="primary" size="small" />
-);
-
-const Submitted = () => <LabelStatusSubmit className="primary" size="small" />;
-
-const Deleted = () => <LabelStatusDelete className="negative" size="small" />;
-
-const Accepted = () => <LabelStatusAccept className="positive" size="small" />;
-
-const Declined = () => <LabelStatusDecline className="negative" size="small" />;
-
-const Cancelled = () => <LabelStatusCancel className="neutral" size="small" />;
-
-const Expired = () => <LabelStatusExpire className="expired" size="small" />;
 
 const defaultComponents = {
   [`${appName}.BucketAggregation.element`]: ContribBucketAggregationElement,
@@ -116,21 +63,7 @@ const defaultComponents = {
   [`${appName}.SearchApp.results`]: RequestsResults,
   [`${appName}.SearchBar.element`]: RecordSearchBarElement,
   [`${appName}.EmptyResults.element`]: RequestsEmptyResultsWithState,
-  [`RequestTypeLabel.layout.community-submission`]: CommunitySubmission,
-  [`RequestTypeLabel.layout.community-inclusion`]: CommunityInclusion,
-  [`RequestTypeLabel.layout.community-invitation`]: CommunityInvitation,
-  [`RequestStatusLabel.layout.submitted`]: Submitted,
-  [`RequestStatusLabel.layout.deleted`]: Deleted,
-  [`RequestStatusLabel.layout.accepted`]: Accepted,
-  [`RequestStatusLabel.layout.declined`]: Declined,
-  [`RequestStatusLabel.layout.cancelled`]: Cancelled,
-  [`RequestStatusLabel.layout.expired`]: Expired,
-  [`RequestActionModalTrigger.accept`]: RequestAcceptModalTriggerWithConfig,
-  [`RequestActionModalTrigger.decline`]: RequestDeclineModalTriggerWithConfig,
-  [`RequestActionModalTrigger.cancel`]: RequestCancelModalTriggerWithConfig,
-  [`RequestActionButton.cancel`]: RequestCancelButton,
-  [`RequestActionButton.decline`]: RequestDeclineButton,
-  [`RequestActionButton.accept`]: RequestAcceptButton,
+  ...defaultContribComponents
 };
 
 const overriddenComponents = overrideStore.getAll();
