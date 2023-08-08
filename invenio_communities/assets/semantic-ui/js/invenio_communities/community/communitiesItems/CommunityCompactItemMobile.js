205576dee441df60359833009f0f8a878024f6e4
Jenny Bonsak <jenny.bonsak@gmail.com>
2023-08-08 15:54:42 +0200

    CommunityCompactItem: Fix responsive issues with layout

--- invenio_communities/assets/semantic-ui/js/invenio_communities/community/communitiesItems/CommunityCompactItemMobile.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/community/communitiesItems/CommunityCompactItemMobile.js
@@ -4,57 +4,98 @@
 // Invenio App RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
+import { i18next } from "@translations/invenio_app_rdm/i18next";
 import { CommunityTypeLabel } from "../labels";
 import { RestrictedLabel } from "../labels";
 import _truncate from "lodash/truncate";
 import React from "react";
-import { Image } from "react-invenio-forms";
-import { Item } from "semantic-ui-react";
+import { Image, InvenioPopup } from "react-invenio-forms";
+import { Icon } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
-export const CommunityCompactItemMobile = ({ result, actions }) => {
+export const CommunityCompactItemMobile = ({
+  result,
+  actions,
+  extraLabels,
+  itemClassName,
+  showPermissionLabel,
+}) => {
   const communityType = result.ui?.type?.title_l10n;
-
+  const { metadata, ui, links, access, id } = result;
   return (
-    <Item key={result.id} className="mobile only justify-space-between">
-      <Image size="mini" src={result.links.logo} alt="" />
+    <div key={id} className={`community-item mobile only ${itemClassName}`}>
+      <div className="display-grid auto-column-grid no-wrap">
+        <div className="flex align-items-center">
+          <Image
+            wrapped
+            size="mini"
+            src={links.logo}
+            alt=""
+            className="community-image rel-mr-1"
+          />
 
-      <Item.Content verticalAlign="middle">
-        <Item.Header
-          as="a"
-          href={result.links.self_html}
-          className="ui small header flex align-items-center mb-5"
-        >
-          {result.metadata.title}
-        </Item.Header>
+          <div>
+            <a href={links.self_html} className="ui small header truncate-lines-2">
+              {metadata.title}
+            </a>
+          </div>
+        </div>
 
-        {result.metadata.description && (
-          <Item.Description
-            as="p"
+        <div className="flex align-items-center justify-end">
+          {showPermissionLabel && (
+            <span className="rel-mr-1">
+              {ui?.permissions?.can_include_directly && (
+                <InvenioPopup
+                  popupId="direct-publish-info-popup"
+                  size="small"
+                  trigger={<Icon name="paper plane outline neutral" size="large" />}
+                  ariaLabel={i18next.t("Submission information")}
+                  content={i18next.t(
+                    "Submission to this community does not require review, and will be published immediately."
+                  )}
+                />
+              )}
+            </span>
+          )}
+          {actions}
+        </div>
+      </div>
+
+      <div className="full-width">
+        {metadata.description && (
+          <p
+            className="truncate-lines-1 text size small text-muted mt-5 rel-mb-1"
             dangerouslySetInnerHTML={{
-              __html: _truncate(result.metadata.description, { length: 50 }),
+              __html: _truncate(metadata.description, { length: 50 }),
             }}
           />
         )}
 
-        <Item.Extra>
-          <RestrictedLabel access={result.access.visibility} />
-          <CommunityTypeLabel type={communityType} />
-        </Item.Extra>
-      </Item.Content>
-
-      <div className="flex flex-direction-column align-items-center rel-mt-1">
-        {actions}
+        {(result.access.visibility === "restricted" ||
+          communityType ||
+          extraLabels) && (
+          <div className="rel-mt-1">
+            <RestrictedLabel access={access.visibility} />
+            <CommunityTypeLabel type={communityType} />
+            {extraLabels}
+          </div>
+        )}
       </div>
-    </Item>
+    </div>
   );
 };
 
 CommunityCompactItemMobile.propTypes = {
   result: PropTypes.object.isRequired,
+  extraLabels: PropTypes.node,
+  itemClassName: PropTypes.string,
+  showPermissionLabel: PropTypes.bool,
   actions: PropTypes.node,
 };
 
 CommunityCompactItemMobile.defaultProps = {
   actions: undefined,
+  extraLabels: undefined,
+  itemClassName: "",
+  showPermissionLabel: false,
 };
