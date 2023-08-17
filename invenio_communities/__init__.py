bba7deb30908a1ca8781a059fda3f36ea96d17ac
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-08-17 17:57:02 +0200

    release: v7.5.0

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "7.4.0"
+__version__ = "7.5.0"
 
 __all__ = ("InvenioCommunities", "current_communities")
