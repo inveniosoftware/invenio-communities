6a0be063d7ca8eac3bf064e60560d56b27ddf425
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-09-20 13:16:36 +0200

    user moderation: delete a user's communities on block
    * when a user gets blocked, delete all communities of which they are the
      only owner

--- invenio_communities/requests/user_moderation/actions.py
+++ invenio_communities/requests/user_moderation/actions.py
@@ -1,26 +1,114 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023 CERN.
+# Copyright (C) 2023 TU Wien.
 #
 # Invenio-communities is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 """Communities user moderation actions."""
 
+from collections import defaultdict
+
 from invenio_access.permissions import Identity, system_identity
-from invenio_records_resources.services.uow import RecordIndexOp, unit_of_work
+from invenio_pidstore.errors import PIDDoesNotExistError
 from invenio_search.engine import dsl
+from invenio_vocabularies.proxies import current_service as vocab_service
 
 from invenio_communities.proxies import current_communities
 
+from ...communities.records.systemfields.deletion_status import (
+    CommunityDeletionStatusEnum,
+)
+
+
+def _get_communities_for_user(user_id):
+    """Helper function for getting all communities with the user as the sole owner.
+
+    Note: This function performs DB queries yielding all communities with the given
+    user as the sole owner (which is not hard-limited in the system) and performs
+    service calls on each of them. Thus, this function has the potential of being a very
+    heavy operation, and should not be called as part of the handling of an
+    HTTP request!
+    """
+    comm_cls = current_communities.service.record_cls
+    comm_model_cls = comm_cls.model_cls
+    mem_cls = current_communities.service.members.record_cls
+    mem_model_cls = mem_cls.model_cls
+
+    # collect the owners for each community
+    comm_owners = defaultdict(list)
+    for comm_owner in [
+        mem_cls(m.data, model=m)
+        for m in mem_model_cls.query.filter(mem_model_cls.role == "owner").all()
+    ]:
+        comm_owners[comm_owner.community_id].append(comm_owner)
+
+    # filter for communities that are owned solely by the user in question
+    relevant_comm_ids = [
+        comm_id
+        for comm_id, owners in comm_owners.items()
+        if len(owners) == 1 and str(owners[0].user_id) == user_id
+    ]
+
+    # resolve the communities in question
+    communities = [
+        comm_cls(m.data, model=m)
+        for m in comm_model_cls.query.filter(
+            comm_model_cls.id.in_(relevant_comm_ids)
+        ).all()
+    ]
+
+    return communities
+
 
 def on_block(user_id, uow=None, **kwargs):
-    """Removes communities that belong to a user."""
-    pass
+    """Removes records that belong to a user.
+
+    Note: This function operates on all records of a user and thus has the potential
+    to be a very heavy operation! Thus it should not be called as part of the handling
+    of an HTTP request!
+    """
+    user_id = str(user_id)
+    tombstone_data = {"note": "User was blocked"}
+
+    # set the removal reason if the vocabulary item exists
+    try:
+        removal_reason_id = kwargs.get("removal_reason_id", "misconduct")
+        vocab = vocab_service.read(
+            identity=system_identity, id_=("removalreasons", removal_reason_id)
+        )
+        tombstone_data["removal_reason"] = {"id": vocab.id}
+    except PIDDoesNotExistError:
+        pass
+
+    # soft-delete all the published records of that user
+    for comm in _get_communities_for_user(user_id):
+        if not comm.deletion_status.is_deleted:
+            current_communities.service.delete_community(
+                system_identity,
+                comm.pid.pid_value,
+                tombstone_data,
+                uow=uow,
+            )
 
 
 def on_restore(user_id, uow=None, **kwargs):
-    """Restores communities that belong to a user."""
-    pass
+    """Restores records that belong to a user.
+
+    Note: This function operates on all records of a user and thus has the potential
+    to be a very heavy operation! Thus it should not be called as part of the handling
+    of an HTTP request!
+    """
+    user_id = str(user_id)
+
+    # restore all the deleted records of that user
+    for comm in _get_communities_for_user(user_id):
+        if comm.deletion_status == CommunityDeletionStatusEnum.DELETED:
+            current_communities.service.restore_community(
+                system_identity,
+                comm.pid.pid_value,
+                uow=uow,
+            )
 
 
 def on_approve(user_id, uow=None, **kwargs):
