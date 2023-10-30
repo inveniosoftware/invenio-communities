a5b42474e3fa857f181c1e78d8a6e63f39fb653b
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2023-10-30 14:53:14 +0100

    Add overridable tags and  overridable context to community profile form.

--- invenio_communities/assets/semantic-ui/js/invenio_communities/settings/profile/index.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/settings/profile/index.js
@@ -10,6 +10,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { default as CommunityProfileForm } from "./CommunityProfileForm";
+import { OverridableContext, overrideStore } from "react-overridable";
 
 const domContainer = document.getElementById("app");
 const community = JSON.parse(domContainer.dataset.community);
@@ -18,16 +19,19 @@ const types = JSON.parse(domContainer.dataset.types);
 const logoMaxSize = JSON.parse(domContainer.dataset.logoMaxSize);
 const customFields = JSON.parse(domContainer.dataset.customFields);
 const permissions = JSON.parse(domContainer.dataset.permissions);
+const overriddenComponents = overrideStore.getAll();
 
 ReactDOM.render(
-  <CommunityProfileForm
-    community={community}
-    hasLogo={hasLogo}
-    defaultLogo="/static/images/square-placeholder.png"
-    types={types}
-    logoMaxSize={logoMaxSize}
-    customFields={customFields}
-    permissions={permissions}
-  />,
+  <OverridableContext.Provider value={overriddenComponents}>
+    <CommunityProfileForm
+      community={community}
+      hasLogo={hasLogo}
+      defaultLogo="/static/images/square-placeholder.png"
+      types={types}
+      logoMaxSize={logoMaxSize}
+      customFields={customFields}
+      permissions={permissions}
+    />
+  </OverridableContext.Provider>,
   domContainer
 );
