c06b9fe3fd5213fa6dce65efccd05b29a022d367
Guillaume Viger <fenekku@fenekku.com>
2023-11-02 16:03:53 -0400

    release: v4.2.1

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "4.2.0"
+__version__ = "4.2.1"
 
 __all__ = ("InvenioCommunities", "current_communities")
