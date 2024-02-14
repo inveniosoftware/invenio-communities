93099c4b6a4ffd99015218704a5c013062313c1d
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2024-02-14 10:26:50 +0100

    views: fix menu order

--- invenio_communities/config.py
+++ invenio_communities/config.py
@@ -20,7 +20,6 @@ COMMUNITIES_ROUTES = {
     "frontpage": "/communities",
     "search": "/communities/search",
     "new": "/communities/new",
-    "details": "/communities/<pid_value>",
     "upload": "/communities/<pid_value>/upload",
     "settings": "/communities/<pid_value>/settings",
     "requests": "/communities/<pid_value>/requests",
