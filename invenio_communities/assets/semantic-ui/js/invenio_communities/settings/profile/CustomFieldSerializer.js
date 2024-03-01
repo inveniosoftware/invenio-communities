2d2037172d276c1180f04156b8d111e3c984e719
Fatimah <fatimah.zulfiqar@cern.ch>
2024-03-01 10:37:17 +0100

    custom_fields: added custom vocab flag

--- invenio_communities/assets/semantic-ui/js/invenio_communities/settings/profile/CustomFieldSerializer.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/settings/profile/CustomFieldSerializer.js
@@ -16,28 +16,39 @@ export class CustomFieldSerializer {
     serializedDefault = null,
     allowEmpty = false,
     vocabularyFields = [],
+    genericVocabularies = [],
   }) {
     this.fieldpath = fieldpath;
     this.deserializedDefault = deserializedDefault;
     this.serializedDefault = serializedDefault;
     this.allowEmpty = allowEmpty;
     this.vocabularyFields = vocabularyFields;
+    this.genericVocabularies = genericVocabularies;
   }
 
   #mapCustomFields(record, customFields, mapValue) {
     if (customFields !== null) {
       for (const [key, value] of Object.entries(customFields)) {
         const isVocabularyField = this.vocabularyFields.includes(key);
+        const isGenericVocabulary = this.genericVocabularies.includes(key);
         const _value = _isArray(value)
-          ? value.map((v, i) => mapValue(v, i, isVocabularyField))
-          : mapValue(value, null, isVocabularyField);
+          ? value.map((v, i) => mapValue(v, i, isVocabularyField, isGenericVocabulary))
+          : mapValue(value, null, isVocabularyField, isGenericVocabulary);
         record = _set(record, `custom_fields.${key}`, _value);
       }
     }
   }
 
   deserialize(record) {
-    const _deserialize = (value, i = undefined, isVocabulary = false) => {
+    const _deserialize = (
+      value,
+      i = undefined,
+      isVocabulary = false,
+      isGenericVocabulary = false
+    ) => {
+      if (isVocabulary && !isGenericVocabulary) {
+        return value;
+      }
       if (isVocabulary && value?.id) {
         return value.id;
       }
