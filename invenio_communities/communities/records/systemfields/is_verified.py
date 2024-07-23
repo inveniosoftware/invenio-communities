7be98d2114b2c3a23fe6c4bf2629747bdcc5324c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-07-23 09:50:08 +0200

    fix(verified): the owner is not exclusively a user
    * a group could also be the first owner of a community and therfore
      break the key access

--- invenio_communities/communities/records/systemfields/is_verified.py
+++ invenio_communities/communities/records/systemfields/is_verified.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023-2024 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio-Communities is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -23,11 +24,12 @@ class IsVerifiedField(CalculatedIndexedField):
         # import here due to circular import
         from invenio_communities.members.records.api import Member
 
-        community_verified = False
         owners = [m.dumps() for m in Member.get_members(record.id) if m.role == "owner"]
+
+        # community is considered verified if at least one owner is verified
+        # a user is verified if verified_at is not None
         for owner in owners:
-            # community is considered verified if at least one owner is verified
-            if owner["user"]["verified_at"] is not None:
-                community_verified = True
-                break
-        return community_verified
+            if "user" in owner and owner["user"]["verified_at"] is not None:
+                return True
+
+        return False
