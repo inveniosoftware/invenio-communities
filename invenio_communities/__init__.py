263da5afe52d876da40788043826997b6fec65c9
Javier Romero Caatro <jrcastro9515gmail.com>
2024-08-26 16:25:11 +0200

    release: v15.0.0

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "14.10.0"
+__version__ = "15.0.0"
 
 __all__ = ("InvenioCommunities", "current_communities")
