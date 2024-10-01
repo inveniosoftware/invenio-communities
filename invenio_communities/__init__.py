c22463d78d2cabb92a23e3dd2a92954b1ae0a2ba
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2024-10-01 13:00:16 +0200

    release: v15.2.1

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "15.2.0"
+__version__ = "15.2.1"
 
 __all__ = ("InvenioCommunities", "current_communities")
