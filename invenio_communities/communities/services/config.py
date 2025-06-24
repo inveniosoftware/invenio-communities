93e0b9f8b8812eeb7286025dafc1f6124d4e13ee
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2025-06-24 22:07:49 +0200

    feat (services): allow custom schema and record class

--- invenio_communities/communities/services/config.py
+++ invenio_communities/communities/services/config.py
@@ -97,7 +97,7 @@ class CommunityServiceConfig(RecordServiceConfig, ConfiguratorMixin):
         "COMMUNITIES_PERMISSION_POLICY", default=CommunityPermissionPolicy
     )
     # Record specific configuration
-    record_cls = Community
+    record_cls = FromConfig("COMMUNITIES_COMMUNITY_CLS", default=Community)
     result_item_cls = CommunityItem
     result_list_cls = CommunityListResult
     indexer_queue_name = "communities"
@@ -111,7 +111,7 @@ class CommunityServiceConfig(RecordServiceConfig, ConfiguratorMixin):
     )
 
     # Service schema
-    schema = CommunitySchema
+    schema = FromConfig("COMMUNITIES_COMMUNITY_SCHEMA", default=CommunitySchema)
     schema_featured = CommunityFeaturedSchema
     schema_tombstone = TombstoneSchema
 
