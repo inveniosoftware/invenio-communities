ce693362e27ddaa2482e3b87d046442b9f68b4c8
Sam Arbid <samaa2@kth.se>
2025-06-26 02:17:51 +0200

    i18n: Remove unnecessary Trans components from member selection
    *  props passed down from parent components should already be translated
        at the parent level before being passed as props
    * fix Unsupported JSX expression warnings when running npm run extract_messages

--- invenio_communities/assets/semantic-ui/js/invenio_communities/members/components/SearchWithRoleSelection.js
+++ invenio_communities/assets/semantic-ui/js/invenio_communities/members/components/SearchWithRoleSelection.js
@@ -11,7 +11,6 @@ import { ErrorMessage } from "@js/invenio_communities/members/components/ErrorMe
 import { i18next } from "@translations/invenio_communities/i18next";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { Trans } from "react-i18next";
 import { Button, Form, Modal } from "semantic-ui-react";
 import { MembersSearchBar } from "../invitations/invitationsModal/MemberSearchBar";
 import { SelectedMembers } from "./bulk_actions/SelectedMembers";
@@ -126,9 +125,9 @@ export class SearchWithRoleSelection extends Component {
             onClick={modalClose}
           />
           {selectedCount > 0 && (
-            <Trans key="entitiesSelected" count={selectedCount}>
-              {doneButtonTip} {{ selectedCount }} {doneButtonTipType}
-            </Trans>
+            <>
+              {doneButtonTip} {selectedCount} {doneButtonTipType}
+            </>
           )}
           <Button
             content={doneButtonText}
