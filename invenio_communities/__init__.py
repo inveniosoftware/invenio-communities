1440b8ed97d5a199949e2f00c6f6cd3e447184a7
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-07-17 20:21:24 +0200

    release: v19.2.0

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -12,6 +12,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "19.1.0"
+__version__ = "19.2.0"
 
 __all__ = ("InvenioCommunities", "current_communities")
