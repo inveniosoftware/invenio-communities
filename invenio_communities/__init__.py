bb74b51490c7dac86cb806630c0af68fc8fc6330
Sam Arbid <samaa2@kth.se>
2025-10-29 11:10:05 +0100

    📦 release: v19.2.3

--- invenio_communities/__init__.py
+++ invenio_communities/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of Invenio.
 # Copyright (C) 2016-2025 CERN.
 # Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,6 +13,6 @@
 from .ext import InvenioCommunities
 from .proxies import current_communities
 
-__version__ = "19.2.2"
+__version__ = "19.2.3"
 
 __all__ = ("InvenioCommunities", "current_communities")
