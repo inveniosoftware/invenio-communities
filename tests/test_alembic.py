be32bb6c279ec2732dc95dfe85c058269fd24880
Miroslav Simek <3385642+mesemus@users.noreply.github.com>
2026-04-26 15:24:52 +0200

    feat: added alembic tests

---
+++ tests/test_alembic.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2026 CESNET z.s.p.o.
+#
+# Invenio-communities is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""Test invenio-communities alembic."""
+
+import pytest
+from invenio_db.utils import alembic_test_context, drop_alembic_version_table
+
+
+def test_alembic(base_app, database):
+    """Test alembic recipes."""
+    db = database
+    ext = base_app.extensions["invenio-db"]
+
+    if db.engine.name == "sqlite":
+        raise pytest.skip("Upgrades are not supported on SQLite.")
+
+    base_app.config["ALEMBIC_CONTEXT"] = alembic_test_context()
+
+    # Check that this package's SQLAlchemy models have been properly registered
+    tables = [x for x in db.metadata.tables]
+    assert "communities_metadata" in tables
+    assert "communities_members" in tables
+
+    # Check that Alembic agrees that there's no further tables to create.
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    # Drop everything and recreate tables all with Alembic
+    db.drop_all()
+    drop_alembic_version_table()
+    ext.alembic.upgrade()
+    for m in ext.alembic.compare_metadata():
+        print("Missing migration for: ", m)
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    drop_alembic_version_table()
