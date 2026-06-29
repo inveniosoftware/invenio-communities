7533d21ac629ca71e112886d3b268705e91cfce6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 22:25:26 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * bump requires-python due to dependencies requiring 3.9+
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- run-tests.sh
+++ run-tests.sh
@@ -48,7 +48,6 @@ if [[ ${keep_services} -eq 0 ]]; then
 	trap cleanup EXIT
 fi
 
-python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-redis} --env)"
 # Note: expansion of pytest_args looks like below to not cause an unbound
